59306cdbab4fc9ed50ba26c380dad98331c3a39e
pravakarcode <pravakaradhikari13@gmail.com>
2023-12-04 19:15:51 +0545

    SearchDone

---
+++ components/SearchForm.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { BedDoubleIcon } from "lucide-react";
+
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+
+export const formSchema = z.object({
+  location: z.string().min(2).max(50),
+  dates: z.object({
+    from: z.date(),
+    to: z.date(),
+  }),
+  adults: z
+    .string()
+    .min(1, { message: "Please select at least 1 adult" })
+    .max(12, { message: "Max 12 adults Occupancy" }),
+  children: z.string().min(0).max(12, {
+    message: "Max 12 children Occupancy",
+  }),
+  rooms: z.string().min(1, {
+    message: "Please select at least 1 room",
+  }),
+});
+
+const SearchForm = () => {
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      location: "",
+      dates: {
+        from: undefined,
+        to: undefined,
+      },
+      adults: "1",
+      children: "0",
+      rooms: "1",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {}
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col lg:flex-row lg:max-w-6x lg:mx-auto items-center justify-center space-x-0 lg:space-x-2 space-y-4 lg:space-y-0 rounded-lg"
+      >
+        <div className="grid w-full lg:max-w-sm items-center gap-1.5">
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-white flex">
+                  Location
+                  <BedDoubleIcon className="ml-2 h-4 w-4 text-white" />
+                </FormLabel>
+
+                <FormMessage />
+
+                <FormControl>
+                  <Input placeholder="London, UK" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="grid w-full lg:max-w-sm flex-1 items-center gap-1.5">
+          <FormField
+            control={form.control}
+            name="dates"
+            render={({ field }) => (
+              <FormItem className="flex flex-col">
+                <FormLabel className="text-white">Dates</FormLabel>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default SearchForm;
---
+++ data/trending.ts
@@ -0,0 +1,38 @@
+export const trending_data = [
+    {
+        id:1,
+        src: "https://cf.bstatic.com/xdata/images/hotel/square600/213805317.webp?k=70f1f22fa9f51f4650eb6642b89f6e4c43f231f77d2686d0e019348042896c54&o=",
+        title: "Dubai",
+        location: "Dubai Autodrome",
+        description: "15 Deals"
+    },
+    {
+        id:2,
+        src: "https://cf.bstatic.com/xdata/images/hotel/square600/507815052.webp?k=2035005700330fade18a61dfe05cf80c2d444b9b9c7433fdf187197bed8b4663&o=",
+        title: "UK",
+        location: "New York",
+        description: "32 Deals"
+    },
+    {
+        id:3,
+        src: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/407354746.jpg?k=c565f00cce33ad782ad358bdf9ebd76875312d96df2013086dacd8aa39f0f571&o=&hp=1",
+        title: "Nepal",
+        location: "Kathmandu",
+        description: "1 Deals"
+    },
+    {
+        id:4,
+        src: "https://cf.bstatic.com/xdata/images/hotel/square600/503272848.webp?k=54f08ef0d675e7b55fc159a7edbd329275416f826c4d532b55a1393c581def0a&o=",
+        title: "India",
+        location: "New Delhi",
+        description: "17 Deals"
+    },
+    {
+        id:5,
+        src: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/440057339.jpg?k=33a185c455507e9c97e6deab14b394d63e7074a1ae7f2e9f42da9ed6379e7e02&o=&hp=1",
+        title: "Australia",
+        location: "Sydney",
+        description: "24 Deals"
+    },
+
+]
