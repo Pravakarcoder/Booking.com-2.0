7ca03344334188181cd8f3b596332eaf2df97d15
pravakarcode <pravakaradhikari13@gmail.com>
2023-12-06 13:47:28 +0545

    Done

---
+++ typings.ts
@@ -0,0 +1,20 @@
+export type Listing  = {
+    url: string
+    title: string
+    rating: string | null
+    description: string
+    price: string
+    link: string
+    booking_metadata: string
+    rating_word: string
+    rating_count: string
+}
+
+
+export type Result = {
+    content: {
+        listings: Listing[]
+        total_listings: string;
+
+    }
+}
